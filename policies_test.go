5f9660f22e0fbf0a8396c73ef2e4d857ba98b681
shawnps <shawn@signalsciences.com>
2016-01-22 15:42:20 -0800

    gofmt -s

--- policies_test.go
+++ policies_test.go
@@ -36,19 +36,19 @@ func TestStrictPolicy(t *testing.T) {
 	p := StrictPolicy()
 
 	tests := []test{
-		test{
+		{
 			in:       "Hello, <b>World</b>!",
 			expected: "Hello, World!",
 		},
-		test{
+		{
 			in:       "<blockquote>Hello, <b>World</b>!",
 			expected: "Hello, World!",
 		},
-		test{ // Real world example from a message board
+		{ // Real world example from a message board
 			in:       `<quietly>email me - addy in profile</quiet>`,
 			expected: `email me - addy in profile`,
 		},
-		test{},
+		{},
 	}
 
 	for ii, test := range tests {
@@ -69,78 +69,78 @@ func TestUGCPolicy(t *testing.T) {
 
 	tests := []test{
 		// Simple formatting
-		test{in: "Hello, World!", expected: "Hello, World!"},
-		test{in: "Hello, <b>World</b>!", expected: "Hello, <b>World</b>!"},
+		{in: "Hello, World!", expected: "Hello, World!"},
+		{in: "Hello, <b>World</b>!", expected: "Hello, <b>World</b>!"},
 		// Blocks and formatting
-		test{
+		{
 			in:       "<p>Hello, <b onclick=alert(1337)>World</b>!</p>",
 			expected: "<p>Hello, <b>World</b>!</p>",
 		},
-		test{
+		{
 			in:       "<p onclick=alert(1337)>Hello, <b>World</b>!</p>",
 			expected: "<p>Hello, <b>World</b>!</p>",
 		},
 		// Inline tags featuring globals
-		test{
+		{
 			in:       `<a href="http://example.org/" rel="nofollow">Hello, <b>World</b></a><a href="https://example.org/#!" rel="nofollow">!</a>`,
 			expected: `<a href="http://example.org/" rel="nofollow">Hello, <b>World</b></a><a href="https://example.org/#%21" rel="nofollow">!</a>`,
 		},
-		test{
+		{
 			in:       `Hello, <b>World</b><a title="!" href="https://example.org/#!" rel="nofollow">!</a>`,
 			expected: `Hello, <b>World</b><a title="!" href="https://example.org/#%21" rel="nofollow">!</a>`,
 		},
 		// Images
-		test{
+		{
 			in:       `<a href="javascript:alert(1337)">foo</a>`,
 			expected: `foo`,
 		},
-		test{
+		{
 			in:       `<img src="http://example.org/foo.gif">`,
 			expected: `<img src="http://example.org/foo.gif">`,
 		},
-		test{
+		{
 			in:       `<img src="http://example.org/x.gif" alt="y" width=96 height=64 border=0>`,
 			expected: `<img src="http://example.org/x.gif" alt="y" width="96" height="64">`,
 		},
-		test{
+		{
 			in:       `<img src="http://example.org/x.png" alt="y" width="widgy" height=64 border=0>`,
 			expected: `<img src="http://example.org/x.png" alt="y" height="64">`,
 		},
 		// Anchors
-		test{
+		{
 			in:       `<a href="foo.html">Link text</a>`,
 			expected: `<a href="foo.html" rel="nofollow">Link text</a>`,
 		},
-		test{
+		{
 			in:       `<a href="foo.html" onclick="alert(1337)">Link text</a>`,
 			expected: `<a href="foo.html" rel="nofollow">Link text</a>`,
 		},
-		test{
+		{
 			in:       `<a href="http://example.org/x.html" onclick="alert(1337)">Link text</a>`,
 			expected: `<a href="http://example.org/x.html" rel="nofollow">Link text</a>`,
 		},
-		test{
+		{
 			in:       `<a href="https://example.org/x.html" onclick="alert(1337)">Link text</a>`,
 			expected: `<a href="https://example.org/x.html" rel="nofollow">Link text</a>`,
 		},
-		test{
+		{
 			in:       `<a href="HTTPS://example.org/x.html" onclick="alert(1337)">Link text</a>`,
 			expected: `<a href="https://example.org/x.html" rel="nofollow">Link text</a>`,
 		},
-		test{
+		{
 			in:       `<a href="//example.org/x.html" onclick="alert(1337)">Link text</a>`,
 			expected: `<a href="//example.org/x.html" rel="nofollow">Link text</a>`,
 		},
-		test{
+		{
 			in:       `<a href="javascript:alert(1337).html" onclick="alert(1337)">Link text</a>`,
 			expected: `Link text`,
 		},
-		test{
+		{
 			in:       `<a name="header" id="header">Header text</a>`,
 			expected: `<a id="header">Header text</a>`,
 		},
 		// Tables
-		test{
+		{
 			in: `<table style="color: rgb(0, 0, 0);">` +
 				`<tbody>` +
 				`<tr>` +
@@ -170,12 +170,12 @@ func TestUGCPolicy(t *testing.T) {
 				`</table>`,
 		},
 		// Ordering
-		test{
+		{
 			in:       `xss<a href="http://www.google.de" style="color:red;" onmouseover=alert(1) onmousemove="alert(2)" onclick=alert(3)>g<img src="http://example.org"/>oogle</a>`,
 			expected: `xss<a href="http://www.google.de" rel="nofollow">g<img src="http://example.org"/>oogle</a>`,
 		},
 		// OWASP 25 June 2014 09:15 Strange behaviour
-		test{
+		{
 			in:       "<table>Hallo\r\n<script>SCRIPT</script>\nEnde\n\r",
 			expected: "<table>Hallo\n\nEnde\n\n",
 		},
