f9db59de95bdbdbd758e1ff2ccbdebab16094fee
guoguangwu <guoguangwu@magic-shield.com>
2023-10-12 10:50:37 +0800

    chore: remove refs to deprecated io/ioutil
    Signed-off-by: guoguangwu <guoguangwu@magic-shield.com>

--- cmd/sanitise_ugc/main.go
+++ cmd/sanitise_ugc/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"os"
 
@@ -22,7 +22,7 @@ func main() {
 
 	// Read input from stdin so that this is a nice unix utility and can receive
 	// piped input
-	dirty, err := ioutil.ReadAll(os.Stdin)
+	dirty, err := io.ReadAll(os.Stdin)
 	if err != nil {
 		log.Fatal(err)
 	}
